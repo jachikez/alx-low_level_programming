5ebafba8188a4d022c941ca8c5ffe2970cb86b9d
jachikez <aneloekwueme@gmail.com>
2021-06-17 22:53:01 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
 /**
+ *
  *  * main - Entry point
+ *
  *   *
+ *
  *    * Return: Always 0 (Success)
+ *
  *     */
-int main(void)
-{
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
-	return (0);
-}
 
+int main(void)
+
+{
+
+		fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+						59, 1, stderr);
+
+			return (1);
+
+}
