a192fe169044e88b92792d7d444a847b8e941817
jachikez <aneloekwueme@gmail.com>
2021-06-17 23:07:06 -0700

    update on c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,11 +13,10 @@
 int main(void)
 
 {
-<<<<<<< HEAD
+
 		fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 						59, 1, stderr);
 			return (1);
-=======
 
 		fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 
@@ -25,5 +24,4 @@ int main(void)
 
 			return (1);
 
->>>>>>> 5ebafba8188a4d022c941ca8c5ffe2970cb86b9d
 }
