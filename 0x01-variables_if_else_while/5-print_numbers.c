57da34f9ccdb569f1da7a20a0707a42e7150636a
jachikez <aneloekwueme@gmail.com>
2021-06-19 02:00:43 -0700

    new project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * Description: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+printf("%d", digit);
+printf("\n");
+return (0);
+}
